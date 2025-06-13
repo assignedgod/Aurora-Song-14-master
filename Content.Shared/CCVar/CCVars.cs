ed53e4d2f0484ade10b4aab9dbd7de6c5097cf6b
assignedgod <79281911+assignedgod@users.noreply.github.com>
2025-06-13 01:05:26 -0400

    Update CCVars.cs
    Re-add silicon update time

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -41,6 +41,9 @@ public sealed partial class CCVars : CVars
     /// <summary>
     ///     Should the player automatically get up after being knocked down
     /// </summary>
+   public static readonly CVarDef<float> SiliconNpcUpdateTime =
+        CVarDef.Create("silicon.npcupdatetime", 1.5f, CVar.SERVERONLY);
+    
     public static readonly CVarDef<bool> AutoGetUp =
         CVarDef.Create("white.auto_get_up", true, CVar.CLIENT | CVar.ARCHIVE | CVar.REPLICATED); // WD EDIT
 }
